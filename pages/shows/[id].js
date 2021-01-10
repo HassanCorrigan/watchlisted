458acbc046e9b9a6508ae92856e42006ddd037f0
Hassan Corrigan <corriganhassan@gmail.com>
2021-01-10 23:39:12 +0000

    Add content to single tv show page, update styles

--- pages/shows/[id].js
+++ pages/shows/[id].js
@@ -1,6 +1,8 @@
+import Link from 'next/link';
 import { tmdbFetch } from '../../helpers/apiFetch.js';
 import Layout from '../../components/Layout.js';
 import styles from '../../styles/single-item.module.css';
+import seasonStyles from '../../styles/horizontal-poster-list.module.css';
 
 const Show = ({ show }) => {
   console.log(show);
@@ -17,22 +19,43 @@ const Show = ({ show }) => {
           alt={show.name}
           className={styles.poster}
         />
-        <p>Aired: {show.first_air_date}</p>
-        {show.networks.map(network => (
-          <p key={network.id}>{network.name}</p>
-        ))}
-
-        {show.number_of_seasons <= 1 ? (
-          <p>{show.number_of_seasons} Season</p>
-        ) : (
-          <p>{show.number_of_seasons} Seasons</p>
-        )}
+        <p>Run Time: {show.episode_run_time[0]} mins</p>
+        <p>Aired: {show.first_air_date.slice(0, 4)}</p>
+        <p>
+          {show.networks.map(network => (
+            <span className='tag' key={network.id}>
+              {network.name}{' '}
+            </span>
+          ))}
+        </p>
 
         <p>{show.overview}</p>
 
+        <div className={styles.seasonsOverview}>
+          {show.number_of_seasons <= 1 ? (
+            <h3>{show.number_of_seasons} Season</h3>
+          ) : (
+            <p>{show.number_of_seasons} Seasons</p>
+          )}
+          <div className={seasonStyles.posterList}>
+            {show.seasons.map(season => (
+              <a href={`/shows/${show.id}/season/${season.id}`} key={season.id}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w200/${season.poster_path}`}
+                  alt={season.name}
+                  className={seasonStyles.poster}
+                />
+                <p className={seasonStyles.posterTitle}>{season.name}</p>
+              </a>
+            ))}
+          </div>
+        </div>
+
         <p>
           {show.genres.map(genre => (
-            <span key={genre.id}>{genre.name} </span>
+            <span className='tag' key={genre.id}>
+              {genre.name}
+            </span>
           ))}
         </p>
       </section>
